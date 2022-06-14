ac577e090c0bcb132b7ee1b50b101f1038971678
fahadalim <mohammadfahadalim@gmail.com>
2022-06-14 19:38:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState(0);
   return (
     <div className="App">
-      <h2 data-testid="counter-value">X</h2>
-      <button data-testid="counter-decrement-button"></button>
-      <button data-testid="counter-increment-button"></button>
+      <h2 data-testid="counter-value">{data}</h2>
+      {data !== 0 ? (
+        <button
+          onClick={() => {
+            setData(data - 1);
+          }}
+          data-testid="counter-increment-button"
+        >
+          sub
+        </button>
+      ) : (
+        <button disabled data-testid="counter-increment-button">
+          sub
+        </button>
+      )}
+      <button
+        onClick={() => {
+          setData(data + 1);
+        }}
+        data-testid="counter-decrement-button"
+      >
+        add
+      </button>
     </div>
   );
 }
